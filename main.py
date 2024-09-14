c291b0ee8cb439f221e7e1a2ee54920208a34f95
BefitClock1 <tymofii.pryputa.ir.2024@lpnu.ua>
2024-09-14 11:18:23 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+book="witcher"
+print(book)
+
+is_adult = False
+is_child = True
+print(is_adult and is_child, is_adult or is_child, not is_adult, not is_child)
+
+is_sunny = False
+is_cloudy = False
+print(not is_sunny, not is_cloudy)
+
+
+x=166.124
+z=15.839
+print(1/x + 2/(x**2 + z) + 3/(x**3 + z **2) + 4/(x**4 + z **3) + 5/(x**5 + z **4))
+
